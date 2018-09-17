c5e0cf0465e31e241a7447bf1a3767042a3d18ea
CHAD-SVASTISALEE <SWASGRAFX@GMAIL.COM>
2018-09-17 10:27:08 -0500

    Added Call Production

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ var LocalStrategy = require('passport-local').Strategy;
 //Require models
 var Users = require('./models/users');
 
-var config = require('./config.dev');
+if(process.env.NODE_ENV==='production'){
+  var config = require('../config.prod');
+}else{
+  var config = require('./config.dev');
+}
+
 var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
